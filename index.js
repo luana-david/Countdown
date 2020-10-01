92d8b21dbea1abc857cb4c4718e96ffbc2697369
DESKTOP-KL10C48\Luana <andreea.luana.david@gmail.com>
2020-10-01 14:08:21 +0300

    calculate countdown on click

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 var calcDate = new Date();
 
-$('input#date').change(event => {
-    calcDate = new Date(event.target.value)
-    setInterval(() => {
-        const daysLeft = Math.floor(Math.abs((calcDate - new Date()))/(1000 * 60 *60 *24))
-        const hoursLeft = Math.floor((23 - new Date().getHours()))
-        const minutesLeft = Math.floor((59 - new Date().getMinutes()))
-        const secondsLeft = Math.floor((59 - new Date().getSeconds()))
-        const render = daysLeft + ' ' + hoursLeft + ' ' + minutesLeft + ' ' + secondsLeft
-        $("#result").text(render)
-    }, 1000)
-})
-
 function animation() {
     var el ='Countdown!'
     var elSplit = el.split('')
@@ -127,6 +115,18 @@ $(".prev").on('click', function() {
     }
 
     monthCalc(prevMonth, prevYear)
+})
+
+$(".days").on('click', '#day' , function() {
+    calcDate = new Date(currentYear, currentMonth, $(this).text())
+    setInterval(() => {
+        const daysLeft = Math.floor(Math.abs((calcDate - new Date()))/(1000 * 60 *60 *24))
+        const hoursLeft = Math.floor((23 - new Date().getHours()))
+        const minutesLeft = Math.floor((59 - new Date().getMinutes()))
+        const secondsLeft = Math.floor((59 - new Date().getSeconds()))
+        const render = daysLeft + ' ' + hoursLeft + ' ' + minutesLeft + ' ' + secondsLeft
+        $("#result").text(render)
+    }, 1000)
 })
 
 $(document).on('load', animation())
